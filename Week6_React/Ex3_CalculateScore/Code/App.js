4af24a1d83f247695c530b03d755dbe50a03ea9c
mahijaS <119554048+mahijaS@users.noreply.github.com>
2025-07-28 14:13:22 +0530

    Update App.js

--- Week6_React/Ex3_CalculateScore/Code/App.js
+++ Week6_React/Ex3_CalculateScore/Code/App.js
@@ -4,7 +4,7 @@ import { CalculateScore } from "./Components/CalculateScore";
 function App() {
   return (
     <div>
-      <CalculateScore Name = {"Shalini"}
+      <CalculateScore Name = {"Mahija"}
       School = {"Amalorpavam Higher Secondary School"}
       total = {588}
       goal = {600}
